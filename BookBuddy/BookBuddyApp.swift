9b145ed419e85f8eeb12908201a6820add5c3d88
kashrama <kramachandrula21@yahoo.com>
2026-04-24 20:15:42 -0500

    Initial Commit

---
+++ BookBuddy/BookBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookBuddyApp.swift
+//  BookBuddy
+//
+//  Created by Kashyap Ramachandrula on 4/24/26.
+//
+
+import SwiftUI
+
+@main
+struct BookBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
